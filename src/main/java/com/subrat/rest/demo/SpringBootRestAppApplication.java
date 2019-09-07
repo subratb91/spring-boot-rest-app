5619684ee20c4b0528523954529287ec5a7aa092
Subrat Behera <skb@outlook.in>
2019-09-07 13:07:53 +0530

    Second Test Commit

--- src/main/java/com/subrat/rest/demo/SpringBootRestAppApplication.java
+++ src/main/java/com/subrat/rest/demo/SpringBootRestAppApplication.java
@@ -2,7 +2,7 @@ package com.subrat.rest.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+// TEst comment
 @SpringBootApplication
 public class SpringBootRestAppApplication {
 
